0441cf436bb11d5ade4dbe0732392151bb006586
sim <s_bullado51@hotmail.com>
2023-11-25 17:37:22 +0100

    Display of “previous” and “next” buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import PokemonCard from './components/PokemonCard';
 import '/src/App.css';
 
 const pokemonList = [
 	{
 		name: 'bulbasaur',
-		imgSrc: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+		imgSrc:
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+	},
+	{
+		name: 'charmander',
+		imgSrc:
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+	},
+	{
+		name: 'squirtle',
+		imgSrc:
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+	},
+	{
+		name: 'pikachu',
+		imgSrc:
+			'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
 	},
 	{
 		name: 'mew',
@@ -12,11 +29,37 @@ const pokemonList = [
 ];
 
 function App() {
-	const pokemon = pokemonList[0];
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+	const previousPokemon = () => {
+		if (pokemonIndex > 0) {
+			setPokemonIndex(pokemonIndex - 1);
+		}
+	};
+	const nextPokemon = () => {
+		if (pokemonIndex < pokemonList.length - 1) {
+			setPokemonIndex(pokemonIndex + 1);
+		}
+	};
 
 	return (
 		<div className="App">
-			<PokemonCard pokemon={pokemon} />
+			<div className="container">
+				<div className="card-container">
+					<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+					<div className="button-container">
+						{pokemonIndex > 0 && (
+							<button className="button" onClick={previousPokemon}>
+								précédent
+							</button>
+						)}
+						{pokemonIndex < pokemonList.length - 1 && (
+							<button className="button" onClick={nextPokemon}>
+								suivant
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 
-function PokemonCard(props) {
-	const { pokemon } = props;
+function PokemonCard({ pokemon }) {
 	const pictureDisplay = pokemon.imgSrc ? (
 		<img
 			className="card-img"
